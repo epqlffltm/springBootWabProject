57224fa936e5f180e78b8f65ae725b8443213e84
epqlffltm <dsfake1225@gmail.com>
2025-05-22 19:16:32 +0900

    read 완성

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
@@ -28,6 +32,28 @@ public class ArticleController {
         //2. 리파지토리로 엔티티를 BD에 저장
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
-        return "";
+        return "redirect:/articles/"+saved.getId();
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id="+id);
+        //1. id를 조회해서 데이터 가저오기
+        //Optional<Article> articleEntity=articleRepository.findById(id);
+        Article articleEntity=articleRepository.findById(id).orElse(null);
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("article",articleEntity);
+        //3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model){
+        //1. 모든 데이터 가저오기
+        ArrayList<Article> articlesEntityList=articleRepository.findAll();
+        //2. 모델에 데이터 증록
+        model.addAttribute("articleList", articlesEntityList);
+        //3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -5,11 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor//기본생성자 추가 어노테이션
 @ToString
 @Entity
+@Getter
 public class Article {
     @Id
     @GeneratedValue
@@ -29,6 +33,10 @@ public class Article {
     public String toString(){
         return "Artcle{"+"id="+id+", title='"+title+'\''+",content='"+content+'\''+'}';
     }
-
      */
+    /*
+    public Long getId(){
+        return "";
+    }
+    */
 }
