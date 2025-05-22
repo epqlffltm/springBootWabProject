c09a7f7ef8bc5e5e7ba233df1635b17a60dc0ffe
epqlffltm <dsfake1225@gmail.com>
2025-05-22 16:49:15 +0900

    create 완성

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,21 +1,31 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleFrom;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+    @Autowired
+    private ArticleRepository articleRepository;
     @GetMapping("/articles/new")
     public String newArticleForm(){
         return "articles/new";
     }
 
     @PostMapping("/articles/create")
-    public String createArticle(ArticleFrom from)
-    {
-        System.out.println(from.toString());
+    public String createArticle(ArticleFrom from) {
+        Article article = null;
+        System.out.println(article.toString());
+        //1. DTO를 엔티티로 변환
+        article = from.toEntity();
+        //2. 리파지토리로 엔티티를 BD에 저장
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
         return "";
     }
 }
--- firstproject/src/main/java/com/example/firstproject/dto/ArticleFrom.java
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleFrom.java
@@ -1,5 +1,7 @@
 package com.example.firstproject.dto;
 
+import com.example.firstproject.entity.Article;
+
 public class ArticleFrom {
     private String title;
     private String content;
@@ -15,5 +17,9 @@ public class ArticleFrom {
                 "title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 '}';
+    }
+
+    public Article toEntity() {
+        return new Article(null,title,content);
     }
 }
